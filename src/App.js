7c51e6ca8329265f1c1817a9b669a2f234c2fa83
Ajeet Kumar <ajeetkumar97@gmail.com>
2021-02-05 10:36:00 +0530

    Added Pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import {Button} from 'react-bootstrap';
+import Header from './Header'
 
 function App() {
   return (
     <div className="App">
+       <Header />
       <h1>Welcome</h1>
-      <button>button</button>
-      <Button>Bootstrap Button</Button>
+      
     </div>
   );
 }
